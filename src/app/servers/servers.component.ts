f30e911042bf39774cc7fc09984db8aae46534b5
naji-alhusami <najihussami@gmail.com>
2023-04-09 13:45:09 +0300

    test event

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,10 +7,17 @@ import { Component } from '@angular/core';
 })
 export class ServersComponent {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
 
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
+  }
+
+  ngOnInit() {}
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
   }
 }
