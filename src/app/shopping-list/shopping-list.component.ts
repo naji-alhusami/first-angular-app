58883f7e03d6558c7f70f302ae57f2b4d51c33c0
naji-alhusami <najihussami@gmail.com>
2023-04-12 14:43:12 +0300

    added ingredients model to shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('ing 1', 3),
+    new Ingredient('ing 2', 6),
+  ];
 
+  constructor() {}
 }
